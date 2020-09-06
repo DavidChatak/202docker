8ab67b0156c45fdb4eb651848dc4d172cf55fc8b
david chatak <davidchatak40@gmail.com>
2020-09-06 20:31:27 +0300

    ***

--- build-script.sh
+++ build-script.sh
@@ -7,10 +7,12 @@ sudo usermod -a -G docker ec2-user
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" \
 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-sudo wget https://raw.githubusercontent.com/DavidChatak/202docker/master/bookstore-api.py -P /home/ec2-user
-sudo wget https://raw.githubusercontent.com/DavidChatak/202docker/master/docker-compose.yaml  -P /home/ec2-user
-sudo wget https://raw.githubusercontent.com/DavidChatak/202docker/master/Dockerfile -P /home/ec2-user
-sudo wget https://raw.githubusercontent.com/DavidChatak/202docker/master/build-script.sh -P /home/ec2-user
-sudo wget https://raw.githubusercontent.com/DavidChatak/202docker/master/requirements.txt -P /home/ec2-user
+mkdir 202project
+sudo wget https://raw.githubusercontent.com/DavidChatak/202docker/master/bookstore-api.py -P /home/ec2-user/202project
+sudo wget https://raw.githubusercontent.com/DavidChatak/202docker/master/docker-compose.yaml  -P /home/ec2-user/202project
+sudo wget https://raw.githubusercontent.com/DavidChatak/202docker/master/Dockerfile -P /home/ec2-user/202project
+sudo wget https://raw.githubusercontent.com/DavidChatak/202docker/master/build-script.sh -P /home/ec2-user/202project
+sudo wget https://raw.githubusercontent.com/DavidChatak/202docker/master/requirements.txt -P /home/ec2-user/202project
+cd /home/ec2-user/202project
 docker build -t davidchatak/bookstore-api:latest .
 docker-compose up
